76ead10c0828a93bbe4d16003592d3a42ddbd264
Eric Lingren <ericlingren@gmail.com>
2021-02-25 15:32:31 -0700

    adding portfolio site and styles cleanup

--- src/App.js
+++ src/App.js
@@ -9,9 +9,16 @@ function App() {
           Captian Jack's ADA Stake Pool
         </h1>
         <img src={captianJack} className="jack-image-home" alt="captian-jack" />
-        <p > An ADA stake pool in honor of the late captian Jack. </p>
+        <p> An ADA stake pool in honor of the late captian Jack. </p>
         <p> Dedicated to financial freedom and quality of life for all.</p>
         <p> RIP Capt'n Jack</p>
+        <div className='about-container'>
+          <h3>About Me:</h3>
+          <p> I'm an expereinced software engineer who enjoys all technology and loves building cool things.</p>
+          <p>You can see more of my other work and find my contact info on 
+            <a target='blank' href='https://ericlingren.com'> my portfolio site here.</a>
+          </p>
+        </div>
       </header>
     </div>
   );
